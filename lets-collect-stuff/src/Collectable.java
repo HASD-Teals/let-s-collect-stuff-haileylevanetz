966a85468e141fcc87c13223f699d55245835205
haileylevanetz <haileym.levanetz@gmail.com>
2022-02-17 10:02:20 -0600

    not done

--- lets-collect-stuff/src/Collectable.java
+++ lets-collect-stuff/src/Collectable.java
@@ -1,9 +1,60 @@
 public class Collectable {
-    // Add collectable properties here
-
-    // Add collectable constructors here
-
-    // Add collectable accessors and mutators here
-
-    // Add any methods here
+    // Properties
+    private String brand;
+    private String model;
+    private String color;
+    private String type;
+    private int value;
+    // Constructors
+    public Collectable(){
+        this.brand="";
+        this.model="";
+        this.color="";
+        this.type="";
+        this.value=0;
+    }
+    // Override Constructors
+    public Collectable(String brand, String model, String color, String type, int value){
+        this.brand= brand;
+        this.model=model;
+        this.color=color;
+        this.type=type;
+        this.value=value;
+    }
+    // Accessors
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    // Mutators
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    public void setModel(String model){
+        this.model=model;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public void setValue(int value){
+        this.value=value;
+    }
+    // Methods
+    public String toString(){
+        return this.brand + " " + this.model + " " + this.model + " " + this.type + " " + this.value + " ";
+    }
 }
--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
         // Add your collection ArrayList here..
         // e.g. List<Collectable> collection = new ArrayList<Collectable>();
-
+        List <Collectable> guitars =new ArrayList <Collectable>();
+        createCollection(guitars);
+        System.out.println(guitars);
         // Add collection methods here to manage and manipulate the collection
+    }
+    public static void createCollection(List <Collectable> guitars){
+        Collectable blackSmoke =new Collectable("Martin", "000-17E", "charcoal", "Acoustic-Electric", 1555);
+        Collectable ocean= new Collectable("Gibson", "Les Paul Traditional", "Ocean blue", "Electric", 2650);
+        guitars.add(blackSmoke);
+        guitars.add(ocean);
     }
 }
